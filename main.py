fde9ba7cfc56552d1de2bf781a45cf121bcda2e2
As14n <SnowSneeze@protonmail.com>
2022-09-08 13:08:55 +0530

    csv files

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from mysql.connector import connect, Error  #pip install mysql-connector-python
 from sys import argv
 import time
+import csv
 
 try: f = open("config.txt", "r")
 except FileNotFoundError:
@@ -19,7 +20,7 @@ for line in f:
 
 def log(*args):
     print(time.strftime("[%H:%M:%S] ", time.localtime()), end="")
-    for arg in args: print(arg, end="")
+    for arg in args: print(arg, end=" ")
     print()
 
 field = None
@@ -30,45 +31,89 @@ if(field != None):
     exit()
     
 shouldCreateDB = False
+bookFile = None
 for i in argv:
     arg = i.lower()
     if(arg == "-c"): shouldCreateDB = True
+    if(arg.startswith("-file:")): bookFile = i[len("-file:"):]
 
-def createDB(driver, cmdBuff):
-    log("Creating database...")
+def openDriverAndBuff():
+    log("Opening connection and command buffer")
+    driver = connect(host="localhost", user=username, password=password)
+    return driver, driver.cursor()
+def closeDriverAndBuff(driver, cmdBuff):
+    log("Closing connection and command buffer")
+    cmdBuff.close()
+    driver.close()
+def Q_createDB(driver, cmdBuff):
+    log("Creating a new database")
     cmdBuff.execute("CREATE DATABASE library")
     cmdBuff.execute("USE library")
     cmdBuff.execute('''
     CREATE TABLE books(
-    name VARCHAR(30) NOT NULL PRIMARY KEY,
-    id   INT         NOT NULL
+    name  VARCHAR(100)  NOT NULL PRIMARY KEY,
+    id    INT           NOT NULL
     )
     ''')
     cmdBuff.execute('''
     CREATE TABLE members(
-    name VARCHAR(30) NOT NULL,
-    id   INT         NOT NULL PRIMARY KEY
+    id    INT          NOT NULL PRIMARY KEY,
+    name  VARCHAR(30)  NOT NULL
+    )
+    ''')
+    cmdBuff.execute('''
+    CREATE TABLE bookMeta(
+    id         INT          NOT NULL PRIMARY KEY,
+    genre      VARCHAR(30)  NOT NULL,
+    authors    VARCHAR(30)  NOT NULL,
+    publisher  VARCHAR(20)
     )
     ''')
     driver.commit()
-def dbContextInit(driver, cmdBuff):
+def Q_dbContextInit(driver, cmdBuff):
     log("Setting up database context")
     cmdBuff.execute("USE library")
     driver.commit()
-def insertBook(bookname, id, driver, cmdBuff):
-    cmdBuff.execute("INSERT INTO books VALUES(\""+bookname+"\","+str(id)+")")
+def Q_insertBook(name, id, genre, authors, publisher, driver, cmdBuff):
+    if publisher == "": publisher = "null"
+    log("Inserting book:", [name, id, genre, authors, publisher])
+    cmdBuff.execute("INSERT INTO books VALUES(\""+name+"\","+str(id)+")")
+    cmdBuff.execute("INSERT INTO bookMeta VALUES("+str(id)+",\""+genre+"\",\""+authors+"\",\""+publisher+"\")")
     driver.commit()
+def Q_insertMember(name, id, driver, cmdBuff):
+    log("Inserting member:", name, id)
+    cmdBuff.execute("INSERT INTO members VALUES(\""+name+"\","+str(id)+")")
+    driver.commit()
+def Q_getHighestBookID(driver, cmdBuff):
+    log("Getting highest book id")
+    cmdBuff.execute("SELECT MAX(id) FROM books")
+    result = cmdBuff.fetchall()
+    driver.commit()
+    id = None
+    if result[0][0] == None: id = 0
+    else: id = result[0][0]
+    return id
 
 try:
-    driver = connect(host="localhost", user=username, password=password)
-    cmdBuff = driver.cursor()
+    driver, cmdBuff = openDriverAndBuff()
 
-    if(shouldCreateDB): createDB(driver, cmdBuff)
-    else: dbContextInit(driver, cmdBuff)
+    if(shouldCreateDB): Q_createDB(driver, cmdBuff)
+    else: Q_dbContextInit(driver, cmdBuff)
     
-    log("Closing connection...")
-    cmdBuff.close()
-    driver.close()
+    hid = Q_getHighestBookID(driver, cmdBuff)
+    
+    if(bookFile != None):
+        books = open(bookFile, "r")
+        reader = csv.reader(books)
+        next(reader)
+        for row in reader:
+            genre = row[3]
+            if row[1] == "": continue
+            if genre  == "": genre = row[2]
+            Q_insertBook(row[0], hid, genre, row[1], row[5], driver, cmdBuff)
+            hid += 1
+   
+    closeDriverAndBuff(driver, cmdBuff)
 except Error as e:
     print("\nMYSQL ERROR")
     print(e)
